483285856e2c2a664eafa8db89750b91fe17ed8a
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-03-26 03:46:22 +0300

    add a new route - communications

--- client/src/network.js
+++ client/src/network.js
@@ -2,4 +2,5 @@ import axios from "axios";
 
 const network = axios.create();
 
+// network.post("http://localhost:8080/communications", { body: "asad" });
 export default network;
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,35 +1,21 @@
 const { Router } = require("express");
-const bodyParser = require("body-parser");
 const Ticket = require("../mongo/models/ticket");
 const tickets = Router();
 
-tickets.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-tickets.use(bodyParser.json());
-
-tickets.get("/:searchText", async (req, res) => {
-  const { searchText } = req.params.searchText;
-  try {
-    const requestedTicketsFromDB = await Ticket.find({
-      $or: [
-        { title: { $regex: `${searchText}`, $options: "i" } },
-        { labels: `${searchText}` },
-      ],
-    });
-    res.status(200).send(requestedTicketsFromDB);
-  } catch ({ message }) {
-    res.status(400).send("request failed");
-    console.log(message);
-  }
-});
-
 tickets.get("/", async (req, res) => {
+  const { searchText } = req.query;
   try {
-    const allTickets = await Ticket.find({});
-    res.status(200).send(allTickets);
+    const requestedTickets = await Ticket.find(
+      !searchText
+        ? {}
+        : {
+            $or: [
+              { title: { $regex: `${searchText}`, $options: "i" } },
+              { labels: { $regex: `${searchText}`, $options: "i" } },
+            ],
+          }
+    );
+    res.status(200).send(requestedTickets);
   } catch ({ message }) {
     console.log(message);
     res.status(400).send("request failed");
@@ -39,13 +25,15 @@ tickets.get("/", async (req, res) => {
 tickets.patch("/:tickedId/done", async (req, res) => {
   try {
     const { tickedId } = req.params;
-    const foundTicket = await Ticket.find({ _id: tickedId });
-    const isDone = foundTicket[0].done;
+    const foundTicket = await Ticket.findById(tickedId);
+    const isDone = foundTicket.done;
     if (!isDone) {
-      foundTicket[0].done = true;
-      await Ticket.replaceOne({ _id: tickedId }, foundTicket[0]);
+      foundTicket.done = true;
+      await Ticket.replaceOne({ _id: tickedId }, foundTicket);
+      res.send({ updated: true });
+    } else {
+      res.send({ updated: false });
     }
-    res.send({ updated: true });
   } catch ({ message }) {
     console.log(message);
     res.status(400).send("update failed");
@@ -55,13 +43,15 @@ tickets.patch("/:tickedId/done", async (req, res) => {
 tickets.patch("/:tickedId/undone", async (req, res) => {
   try {
     const { tickedId } = req.params;
-    const foundTicket = await Ticket.find({ _id: tickedId });
-    const isDone = foundTicket[0].done;
+    const foundTicket = await Ticket.findById({ _id: tickedId });
+    const isDone = foundTicket.done;
     if (isDone) {
-      foundTicket[0].done = false;
-      await Ticket.replaceOne({ _id: tickedId }, foundTicket[0]);
+      foundTicket.done = false;
+      await Ticket.replaceOne({ _id: tickedId }, foundTicket);
+      res.send({ updated: true });
+    } else {
+      res.send({ updated: false });
     }
-    res.send({ updated: true });
   } catch ({ message }) {
     console.log(message);
     res.status(400).send("update failed");
