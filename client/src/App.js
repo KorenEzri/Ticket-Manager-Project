6572b55e0dbbb42c2b8c4d42e9ab6cfe8836dfec
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-03-28 05:31:07 +0300

    code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./components/pages/Homepage/index";
-import RegisterPage from "./components/pages/Register/index";
-import Login from "./components/pages/Login/index";
-import Navbar from "./components/Navbar/index";
-import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PrivateRoute from "./components/pages/PrivateRoute/index";
 
 function App() {
   return (
@@ -14,13 +9,7 @@ function App() {
       <div className="main">
         <Router>
           <Switch>
-            <Route path="/register">
-              <RegisterPage />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <PrivateRoute path="/" component={Homepage} exact />
+            <Homepage />
           </Switch>
         </Router>
       </div>
